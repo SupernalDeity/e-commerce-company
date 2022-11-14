c7af4d935312ced27e022875f4a1dcbef383b4be
Jennifer Arnold <canna@macbook-pro.lan>
2022-11-14 13:11:27 -0500

    updated links and buttons

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -148,12 +148,14 @@ const Cart = () => {
                   </section>
 
                   <div className="mt-8 flex justify-end px-4 sm:px-6 lg:px-8">
+                    <a href="/order">
                     <button
-                      type="submit"
+                      type="button"
                       className="rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
                     >
                       Continue to Payment
                     </button>
+                    </a>
                   </div>
                 </form>
               </Dialog.Panel>
